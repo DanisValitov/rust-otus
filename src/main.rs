15ee1745944132608e0a1682e5ff39e8f6f691d0
DanisValitov <fermodil@mail.ru>
2023-03-23 10:18:21 +0300

    smart-house-1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+struct _SmartSocket {
+    is_on: bool,
+    power_consumption: f32,
+    description: str,
+}
+
+impl _SmartSocket {
+    fn _switch(&mut self) {
+        self.is_on = !self.is_on
+    }
+
+    fn _get_description() {
+        todo!()
+    }
+
+
+    fn _get_power_cons() {
+        todo!()
+    }
+}
+
+struct _SmartThermometer {
+    temperature: f32,
+}
+
+impl _SmartThermometer {
+    fn _get_tmp() {
+        todo!()
+    }
+}
+
+
